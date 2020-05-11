194d20c56876d4eed299c70aaa4eb487253bcdc1
kathiavf16 <kathia.vargasfeliz@gmail.com>
2020-05-10 21:21:46 -0400

    narrative_project

--- Narrative_Viz/Bubble.js
+++ Narrative_Viz/Bubble.js
@@ -2,8 +2,8 @@ class Bubble {
 
     constructor(state, setGlobalState) {
       
-      this.width = 700;
-      this.height = 700;
+      this.width = 1200;
+      this.height = 800;
 
       this.svg = d3.select("#bubble")
       .append("svg")
@@ -28,8 +28,16 @@ class Bubble {
   
     draw(state, setGlobalState) {
 
-      var size = d3.scaleLinear().domain(d3.extent(state.topAirlines, d => d.fatalities)).range([10,180]);
-      console.log("size", size.domain())
+      const size = d3.scaleLinear().domain(d3.extent(state.topAirlines, d => d.fatalities)).range([40,200]);
+      const tooltip = d3.select("body").append("div").attr("class", "toolTip");
+      
+      const mousemove = function(d) {
+        tooltip
+        .style("left", d3.event.pageX - 50 + "px")
+        .style("top", d3.event.pageY - 70 + "px")
+        .style("display", "inline-block")
+        .html((d.operator));}
+  
 
       this.defs.selectAll(".logo-pattern")
       .data(state.topAirlines)
@@ -59,11 +67,22 @@ class Bubble {
       //.style("fill-opacity", 0.3)
       .attr("stroke", "#69a2b2")
       .style("stroke-width", 4)
+      .on("mouseover", mousemove)
+      .call(d3.drag() // call specific function when circle is dragged
+           .on("start", dragstarted)
+           .on("drag", dragged)
+           .on("end", dragended));
+      
+      
 
+      d3.select("#commercial").on("click", function(){
+        console.log("hey","you click me")
+        }) 
+      
       var simulation = d3.forceSimulation()
-     .force("center", d3.forceCenter().x(this.width / 2).y(this.height / 2)) // Attraction to the center of the svg area
+     .force("center", d3.forceCenter().x( this.width / 2).y(this.height / 2)) // Attraction to the center of the svg area
      .force("charge", d3.forceManyBody().strength(0.05)) // Nodes are attracted one each other of value is > 0
-     .force("collide", d3.forceCollide().strength(.2).radius(function(d){ return size(d.fatalities)}).iterations(3)); // Force that avoids circle overlapping
+     .force("collide", d3.forceCollide().strength(.2).radius(function(d){ return size(d.fatalities)}).iterations(1)); // Force that avoids circle overlapping
 
      simulation
      .nodes(state.topAirlines)
@@ -72,6 +91,21 @@ class Bubble {
           .attr("cx", function(d){ return d.x; })
           .attr("cy", function(d){ return d.y; })
     });
+
+    function dragstarted(d) {
+      if (!d3.event.active) simulation.alphaTarget(.03).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+    function dragged(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    }
+    function dragended(d) {
+      if (!d3.event.active) simulation.alphaTarget(.03);
+      d.fx = null;
+      d.fy = null;
+    }
       
     }
   }
